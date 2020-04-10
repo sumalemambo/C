d9cb2657b30f8d33591088626213abbee01181bd
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-04-10 13:18:35 -0400

    Create SegundoC.c

---
+++ TutorialC/SegundoC.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int x;
+    x=0;
+    while (x<10){//esto itera hasta que x sea igual a 10
+        x+=1;
+        printf("El valor de x en este momento es de %d\n",x);
+    }
+}

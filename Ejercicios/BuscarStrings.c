7890e5a868b9b1083e60107eec1910a66f1784fc
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-05-09 17:40:27 -0400

    Create BuscarStrings.c

---
+++ Ejercicios/BuscarStrings.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+
+    int contador;
+    FILE *fp;
+    char **p,c[201];
+    p=(char**)malloc(sizeof(char*)*10000000);
+    fp=fopen("S.txt","r");
+
+    if(fp==NULL){
+        printf("Error al abrir el archivo\n");
+        exit(1);
+    }
+    
+    while(fgets(c,201,fp)!=NULL){
+        p[contador]=(char*)malloc(sizeof(char)*201);
+        strcpy(p[contador],c);// c[0]
+        printf("%s",p[contador]);
+        
+        contador+=1;
+    }
+    for(int i;  i < contador ;i++)
+        free((void*)p[i]);
+    fclose(fp);
+    free((void*)p);
+    return 0;
+}

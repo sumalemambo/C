7249a4b2b354c453895b9b975c0d2bebacbebfff
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-05-19 11:16:22 -0400

    Update Test1.c

--- Test1.c
+++ Test1.c
@@ -3,19 +3,17 @@
 #include <stdlib.h>
 
 char **buscar_str(char **S, int n, char *P, int *largo){
-
-    int contador;
+    int contador=0;
     char **arreglo=(char**)malloc(sizeof(char*)*n);
-    printf("%s",S[1]);
     for(int i = 0; i < n; i++){
-        if(strstr(S[i],P)){
-            arreglo[contador]=(char*)malloc(sizeof(char)*(strlen(S[contador])+1));
+        if(strstr(S[i],P)!=NULL){
+            arreglo[contador]=(char*)malloc(sizeof(char)*(strlen(S[i])));
             strcpy(arreglo[contador],S[i]);
             contador++;
         }
     }
-    arreglo = (char**)realloc(arreglo,(contador + 1)*sizeof(char*));
-    *largo = contador + 1;
+    arreglo = (char**)realloc(arreglo,contador*sizeof(char*));
+    *largo = contador;
     return arreglo;
 
 }
@@ -30,12 +28,11 @@ int main(){
     }
     char **arreglo = (char**)malloc(sizeof(char*)*1000000),**arregloEscribir;
     char *cadena = (char *)malloc(sizeof(char)*201);
-    while(fgets(cadena, 201, fp) != NULL){
+    while(fscanf(fp,"%s",cadena)==1){
         arreglo[i] = (char*)malloc(sizeof(char)*(strlen(cadena)+1));
         strcpy(arreglo[i],cadena);
         i++;
     }
-
     arreglo = (char**)realloc(arreglo,sizeof(char*)*i);
     fclose(fp);
 
@@ -44,13 +41,18 @@ int main(){
         printf("Error al abrir el archivo\n");
         exit(1);
     }
-    while(fgets(cadena,201,fp)){
+    while(fscanf(fp,"%s",cadena)==1){
         arregloEscribir=buscar_str(arreglo,i,cadena,&largo);
-        escribo=fopen("SD.txt","w");
+        escribo=fopen(strcat(cadena,".txt"),"w");
+        for(int j=0;j < largo ; j++){
+            fprintf(escribo,"%s\n",arregloEscribir[j]);
+            free((void*)arregloEscribir[j]);
+        }
+        free((void*)arregloEscribir);
+        fclose(escribo);
     }
-    fclose(escribo);
-    for (int b = 0; b < i; b++)
-    {
+    fclose(fp);
+    for (int b = 0; b < i; b++){
         free((void*)arreglo[b]);
     }
     free((void*)arreglo);

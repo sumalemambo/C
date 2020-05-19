23e5f6bbb60317a5d1a61f9776be1744b7675e0d
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-05-18 21:06:04 -0400

    Update Test1.c

--- Test1.c
+++ Test1.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 
 char **buscar_str(char **S, int n, char *P, int *largo){
+
     int contador;
-    char **arreglo=(char**)malloc(sizeof(char*)*(n+1)),**arreglofinal;
+    char **arreglo=(char**)malloc(sizeof(char*)*n);
+    printf("%s",S[1]);
     for(int i = 0; i < n; i++){
         if(strstr(S[i],P)){
             arreglo[contador]=(char*)malloc(sizeof(char)*(strlen(S[contador])+1));
@@ -15,45 +17,38 @@ char **buscar_str(char **S, int n, char *P, int *largo){
     arreglo = (char**)realloc(arreglo,(contador + 1)*sizeof(char*));
     *largo = contador + 1;
     return arreglo;
+
 }
+
 int main(){
 
-    FILE *fp = fopen("S.txt","r");
-    int i = 0;
-    
+    FILE *fp = fopen("S.txt","r"),*escribo;
+    int i = 0,largo; // no olvidar iniciarlo
     if(fp == NULL){
         printf("Error al abrir el archivo\n");
         exit(1);
     }
-
-    char **arreglo = (char**)malloc(sizeof(char*)*1000000);
+    char **arreglo = (char**)malloc(sizeof(char*)*1000000),**arregloEscribir;
     char *cadena = (char *)malloc(sizeof(char)*201);
-    char **arreglop;
-    int *largo;
-    FILE *fpp;
-    
     while(fgets(cadena, 201, fp) != NULL){
-        printf("%s\n",cadena);
-        arreglo[i] = (char*)malloc(sizeof(char)*201);
-        arreglo[i]= (char*)realloc(arreglo[i],(strlen(cadena)+ 1)*sizeof(char));
+        arreglo[i] = (char*)malloc(sizeof(char)*(strlen(cadena)+1));
         strcpy(arreglo[i],cadena);
         i++;
     }
-    arreglo = (char**)realloc(arreglo,(i + 1)*sizeof(char*));
+
+    arreglo = (char**)realloc(arreglo,sizeof(char*)*i);
     fclose(fp);
-    fp = fopen("P.txt","r");
-    
-    while(fgets(cadena, 201, fp)!= NULL){
-        arreglop = buscar_str(arreglo, i + 1, cadena, largo);
-        fpp = fopen(cadena,"w");
-        for (int j = 0; j < largo; j++)
-        {
-            fprintf(fpp, "%s\n", arreglop[j]);
-            free((void*)arreglop[j]);
-        }
-        free((void*)arreglop);
-        fclose(fpp);
+
+    fp=fopen("P.txt","r");
+    if(fp == NULL){
+        printf("Error al abrir el archivo\n");
+        exit(1);
     }
+    while(fgets(cadena,201,fp)){
+        arregloEscribir=buscar_str(arreglo,i,cadena,&largo);
+        escribo=fopen("SD.txt","w");
+    }
+    fclose(escribo);
     for (int b = 0; b < i; b++)
     {
         free((void*)arreglo[b]);

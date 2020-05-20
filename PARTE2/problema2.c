e95da27dc1afe4da2b524fde2a54628b8957cbcb
Eduardo Pino <65093611+azy00@users.noreply.github.com>
2020-05-20 18:13:33 -0400

    Update problema2.c

--- PARTE2/problema2.c
+++ PARTE2/problema2.c
@@ -1,78 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define nCuentas 4
-
-typedef struct {
-    int nroCuenta;
-    int saldo;
-    char nbre[51];
-    char direccion[51];
-} clienteBanco;
-
-void cambioSaldo(clienteBanco *cliente, int cantidad);
-void transferencia(clienteBanco *cliente1, clienteBanco *cliente2, int cantidad);
-
-void actualizarSaldos(char *clientes, char *transacciones){
-
-    FILE *fc = fopen(clientes,"r");
-    FILE *ft = fopen(transacciones,"r");
-
-    if (fc == NULL){
-        printf("No se pudo abrir el archivo: %s\n",clientes);
-        exit (1);
-    }
-
-    if (ft == NULL){
-        printf("No se pudo abrir el archivo: %s\n", transacciones);
-        exit (1);
-    }
-    
-    clienteBanco cliente;
-    clienteBanco *cuentas = (clienteBanco *)malloc(sizeof(clienteBanco)*10000000);
-    int nclientes;
-    char *comando, *clien1, *deposit , *clien2;
-    int c;
-    comando = (char *)malloc(sizeof(char)*3);
-    
-    char *trans = (char *)malloc(sizeof(char)*5);
-
-    while(fread(&cliente, sizeof(clienteBanco), 1, fc)) //Coloca los structs con la informacion en el array cliente
-    {
-        //printf("Numero de cuenta: %d\n", cliente.nroCuenta);
-        //printf("Saldo: %d\n", cliente.saldo);
-        //printf("Nombre: %s\n", cliente.nbre);
-        //printf("Direccion: %s\n", cliente.direccion);
-        //printf("\n");
-        cuentas[nclientes] = cliente;
-        nclientes++;
-    }
-    cuentas = realloc(cuentas,sizeof(clienteBanco)*nclientes);
-    /*for (int i = 0; i < nclientes; i++)  //Verificar archivos en el array de clientes
-    {
-        printf("Numero de cuenta: %d\n", cuentas[i].nroCuenta);
-        printf("Saldo: %d\n", cuentas[i].saldo);
-        printf("Nombre: %s\n", cuentas[i].nbre);
-        printf("Direccion: %s\n", cuentas[i].direccion);
-    }
-    */
-   // TRANSFORMAR DE ASCII A TEXTO
-    
-    fclose(fc);
-    fclose(ft);
-    free((void*)comando);
-    free((void*)cuentas);
-}
-
-int main(){
-    actualizarSaldos("clientes.txt", "transacciones.txt");
-    return 0;
-}
-/////////////////////////////////////////// P R U E B A/////////////////////////////////////////////////////
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define nCuentas 4
 
 typedef struct {
     int nroCuenta;
@@ -122,21 +50,23 @@ void actualizarSaldos(char *clientes, char *transacciones){
         printf("Direccion: %s\n", cuentas[i].direccion);
     }
     */
-   // TRANSFORMAR DE ASCII A TEXTO
-    char *aux1, *aux2, *aux3;
-    while(aux1 != EOF){
-        aux1 = fgetc(ft);
-        aux2 = fgetc(ft);
-        aux3 = fgetc(ft);
-        char * cadena = (char*)malloc(1 + strlen(aux1) + strlen(aux2) + strlen(aux3));
-        strcpy(cadena, aux1);
-        strcat(cadena, aux2);
-        strcat(cadena, aux3);
-        printf("%s", cadena);
-        printf("\n");
-        free((void*)cadena);    
-    }
-    
+   char aux;
+   while(aux != EOF){
+       int numc1,numc2, cant;
+       aux = fgetc(ft);
+       if (aux == '+'){
+           fscanf(ft, "%d %d", &numc1, &cant);
+           printf("%d %d\n", numc1, cant);
+       }
+       if(aux == '-'){           fscanf(ft, "%d %d", &numc1, &cant);
+           printf("%d %d\n", numc1, cant);
+       }
+       if(aux == '>'){
+           fscanf(ft, "%d %d %d", &numc1,&numc2, &cant);
+           printf("%d %d %d\n", numc1, numc2, cant);
+       }
+   }
+
     fclose(fc);
     fclose(ft);
 }

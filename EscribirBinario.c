73adddc1512272005e18d68a764f56698aada300
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-05-22 21:01:46 -0400

    Create EscribirBinario.c

---
+++ EscribirBinario.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int nroCuenta;
+    int saldo;
+    char nbre[51];
+    char direccion[51];
+}clienteBanco;
+
+int main(){
+    clienteBanco aux;
+    FILE *fc = fopen("clientes.txt","w");
+
+    aux.nroCuenta = 1;
+    aux.saldo= 100;
+    strcpy(aux.nbre,"Ignacio");
+    strcpy(aux.direccion,"Fantasilandia");
+    fwrite(&aux,sizeof(clienteBanco),1,fc);
+    printf("%d\n",aux.nroCuenta);
+
+    aux.nroCuenta = 2;
+    aux.saldo= 1000;
+    strcpy(aux.nbre,"Eduardo");
+    strcpy(aux.direccion,"Fantasilandia");
+    fwrite(&aux,sizeof(clienteBanco),1,fc);
+    printf("%d\n",aux.nroCuenta);
+
+    aux.nroCuenta = 3;
+    aux.saldo= 1000;
+    strcpy(aux.nbre,"Sebastian");
+    strcpy(aux.direccion,"Fantasilandia");
+    fwrite(&aux,sizeof(clienteBanco),1,fc);
+    printf("%d\n",aux.nroCuenta);
+
+    aux.nroCuenta = 4;
+    aux.saldo= 1500;
+    strcpy(aux.nbre,"Arroyuelo");
+    strcpy(aux.direccion,"Fantasilandia");
+    fwrite(&aux,sizeof(clienteBanco),1,fc);
+    printf("%d\n",aux.nroCuenta);
+
+    aux.nroCuenta = 10000;
+    aux.saldo= 1500;
+    strcpy(aux.nbre,"Ar");
+    strcpy(aux.direccion,"Fantasilandia");
+    fwrite(&aux,sizeof(clienteBanco),1,fc);
+    printf("%d\n",aux.nroCuenta);
+    fclose(fc);
+
+
+    return 0;
+}

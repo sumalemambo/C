a52d3664b37916cff157a263e17c1d8d5f896c04
sumalemambo <55157948+sumalemambo@users.noreply.github.com>
2020-07-01 19:16:47 -0400

    Update Test.c

--- Tarea2/Test.c
+++ Tarea2/Test.c
@@ -1,86 +1,106 @@
-#include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include "DLList2.c"
+#include <stdlib.h>
+#include "DNlist.c"
 
 int main(){
-    /* tList L;
-    initList(&L);
-    for(int i = 0; i < 10; i++){
-        insert(&L,i);
-        next(&L);
-    }
-    moveToStart(&L);
-    for(int i = 0; i < 10; i++){
-        next(&L); 
-    }
-        0 1 2 3 4 5 6 7 8 9
-        1 2 3 4 5 6 7 8 9 10
 
-        0 1 2 3 4 5 6 7 8 9
-        9 8 7 6 5 4 3 2 1 0
-
-    for(int i = 0; i < 10; i++){
-        prev(&L); 
-    }
-    erase(&L);
-    for(int i = 0 ; i < 9; i++){
-        printf("%d",erase(&L));
-    }
-    clear(&L);
-    deleteList(&L);
-    */
-
-    //File work
+    int M,N;
     FILE* fp = fopen("test22.dat","r");
+    FILE* fs = fopen("test22.out","w");
     if(fp != NULL){
-
-        int M,N;
-        int Val;
-        char Op[7];
-
-        fscanf(fp,"%d\n%d",&M,&N);
-        if(M <= 0 || N<=0){
-            printf("ERROR : El valor M o el valor N del archivo es cero o negativo.\n");
+        fscanf(fp, "%d %d ",&M,&N);
+        if(M <= 0 || N <= 0){
+            printf("ERROR: El numero M y/o N dentro del archivo es/son cero o negativo");
             exit(1);
         }
+        else{
 
-        tList LM;
-        initList(&LM);
-        insert(&LM,1,M);
-        tList LN;
-        initList(&LN);
+            tList MM;
+            tList FM;
+            int value;
+            initList(&MM);
+            initList(&FM);
+            insert(&MM,1,M);
+            char check;
+            char str[7];
+            unsigned int laux1;
+            unsigned int laux2;
+            int True;
 
-        while (fscanf(fp,"%s ",&Op,1) == 1){
-            if(Op[0] == "m"){
-                moveToStart(&LM);
-                for(int i = 0 ; i < lenght(&LM); i++){
-                    if(getValue(&LM) >= Val){
-                        printf("%d ",LM.curr->inicio);
-                        printf("%d \n",LM.curr->final);
-                        append(&LN,frontValue(&LM),frontValue(&LM)+Val);
-                        swap(&LM,Val);
-                        printf("%d ",LN.curr->inicio);
-                        printf("%d \n",LN.curr->final);
-                        printf("%d ",LM.curr->inicio);
-                        printf("%d \n",LM.curr->final);
-                        break;
+            for(int i = 0 ; i < N; i++){
+
+                fscanf(fp,"%c",&check);
+                laux1=lenght(&MM);
+                laux2=lenght(&FM);
+
+                if(check == 'm'){
+
+                    True=0;
+                    fscanf(fp,"%6s",str);
+                    fscanf(fp,"%d ",&value);
+                    moveToStart(&MM);
+
+                    for(unsigned int k = 0 ; k < laux1 ; k++){
+                        if( getValue(&MM) >= value){
+
+                            fprintf(fs,"Bloque de %d bytes asignado a partir del byte %d\n",value,frontValue(&MM));
+                            append(&FM,frontValue(&MM),value+frontValue(&MM)-1);
+                            swap(&MM,value);
+                            True=1;
+                            break;
+                        }
+                        next(&MM);
                     }
-                    next(&LM);
+                    if(True == 0){
+                        fprintf(fs,"Bloque de %d bytes NO puede ser asignado\n",value);
+                    }
+                    
+                }
+                else{
+
+                    fscanf(fp,"%5s",str);
+                    fscanf(fp,"%d ",&value);
+                    moveToStart(&FM);
+
+                    for(unsigned int k = 0; k < laux2 ;k++){
+                        if(frontValue(&FM) == value){
+
+                            fprintf(fs,"Bloque de %d bytes liberado\n",getValue(&FM));
+                            moveToStart(&MM);
+
+                            for(unsigned int l = 0; l < laux1; l++){
+                                if(frontValue(&MM) > value){
+                                    break;
+                                }
+                                next(&MM);
+                            }
+                            insert(&MM,frontValue(&FM),endValue(&FM));
+                            erase(&FM);
+                            unite(&MM);
+                            break;
+                        }
+                        next(&FM);
+                    }
                 }
             }
-            else if(Op == "free"){
 
+            laux2=lenght(&FM);
+            if(laux2 > 0){
+                fprintf(fs,"Quedaron %d bloques sin liberar (%d bytes)",laux2,byte(&FM));
             }
             else{
-                printf("Operacion no valida\n");
+                fprintf(fs,"Toda la memoria dinámica pedida fue liberada");
             }
+
+            deleteList(&MM);
+            deleteList(&FM);
+            fclose(fp);
+            fclose(fs);
         }
-        fclose(fp);
-        deleteList(&LM);
-        deleteList(&LN);
     }
     else{
-        printf("El archivo no existe.\n"); //Placeholder
+        printf("ERROR: El archivo no existe");
+        exit(1);
     }
+    return 0;
 }
